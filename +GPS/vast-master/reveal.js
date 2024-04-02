524c5f10e9c83ba2e2651021f7c97add3506bed3
Miza_Dev <mizaelwgr@gmail.com>
2024-04-02 14:56:19 -0300

    Add files via upload

---
+++ +GPS/vast-master/reveal.js
@@ -0,0 +1,41 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+// topo do site
+revelar.reveal('.efeito-txt-topo',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.img-logo',
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.icone-pg2',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.logo-gps78',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.funciona-02',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.Beneficios-02',
+{
+    duration: 2000,
+    distance: '90px'
+})
+
